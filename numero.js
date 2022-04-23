83810db8710a5aa5091d071295ca22e08ea97acb
Matito-7 <59352894+Matito-7@users.noreply.github.com>
2022-04-23 17:13:25 -0300

    Exemplos da aula 1

---
+++ numero.js
@@ -0,0 +1,22 @@
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operacaoMatematica);
+
+//ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const numeroOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(numeroOperacao)
+
+//NaN -> Not a Number (não é um numero)
+
+const alura = "Alura";
+console.log(alura*primeiroNumero)
---
+++ strings.js
@@ -0,0 +1,19 @@
+const texto1 = "Olá, Mundo!";
+const texto2 = 'Olá, Mundo';
+const senha = "nossaSenhaSuperProtegida123!";
+const stringnumber = "123.256";
+
+// citação usa aspas simples
+
+const citacao = 'o Matheus disse "oi" '
+console.log(citacao)
+
+//concatenar (+)
+
+const nome = "Meu nome é ";
+const meuNome = "Matheus"
+
+console.log(nome+meuNome)
+
+//template string ou template liberal
+
