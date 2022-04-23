cb5384dace782100ce8ab356b72bc6d14e13215b
Matito-7 <59352894+Matito-7@users.noreply.github.com>
2022-04-23 18:23:11 -0300

    Códigos ver e conversões

---
+++ conversões.js
@@ -0,0 +1,14 @@
+//tipo de dado
+// booleanos
+
+//conversão implicita //===
+
+const numero = 456;
+const numerostring = "456a";
+
+//Numeber()
+//String()
+console.log(numero + Number(numerostring));
+
+
+//conversão explicita //==
